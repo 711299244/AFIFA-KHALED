ddba745e66860156c25c1bfbf20725880f308966
711299244 <32100620+711299244@users.noreply.github.com>
2017-09-24 10:08:56 +0100

    Update sketch_sep23a.ino

--- sketch_sep23a/sketch_sep23a.ino
+++ sketch_sep23a/sketch_sep23a.ino
@@ -1,44 +1,41 @@
 #include <math.h>
-#define SENS_PIN 0
-// here to calcul the Temperature by hard method 
-double Temperature(int RawADC) {
- double Temp;
- Temp = log(10000.0*((1024.0/RawADC-1))); 
- Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp ))* Temp );
- Temp = Temp - 273.15;            
- return Temp;
-}
+//I write in this code the hard method to get the Temperature and the easy method to get the Temperature
+//***************************************************************** 
 //open serial port, sets rate to 9600 bps
 void setup() {
 Serial.begin(9600);
+pinMode(A4, INPUT);
 }
+//*********************************************************************
 // method to display the result  on serial monitor 
 void loop() {
-  int i;
-while (i <=1023){ 
-    int  Result=Temperature(analogRead(i));
-    Serial.print( "    The Temperature in Fahrenheit  is :  ");
-   Serial.println(Result);
-   i=i+5;
-   }
-delay(2000);
+  int ADC;    
+    ADC = analogRead(A4);
+    Serial.println(" The ADC value is : ");
+    Serial.println(ADC);
+    Serial.println("The Temperature value is : ");
+    Serial.println(method_to_get_temp(ADC));
+    delay(1000);
+    
+    
 }
-//linearly calcul Temperature
-int method_easy_calcul(int x_value)
-{
+//***********************************************************************
+// here to calcul the Temperature by hard method 
+//double Temperature(int RawADC) {
+ //double Temp;
+// Temp = log(10000.0*((1024.0/RawADC-1))); 
+// Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp ))* Temp );
+// Temp = Temp - 273.15;            
+ //return Temp;
+//}
+//******************************************************************************
+//Easy method to get the Temprature
+int method_to_get_temp(int ADC_vale){
   
- int temp;
- temp=0.1632*x_value-50.514;
- return(temp); 
-  
-  
-  }
-//print method to linearly calcul Temperature
-  void print_method() {
-  
-    int  output=method_easy_calcul(analogRead(SENS_PIN));
-    Serial.print( "    The Temperature   is :  ");
-   Serial.println(output); 
+  int temp;
+ temp=0.1629*ADC_vale-50.489;
+ return(temp);
+}
    
-delay(500);
-}
+    
+   
