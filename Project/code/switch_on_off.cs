5067decfed22fa4d563e0ee0e255d6ff88b88bd3
AFIFA KHALED <32100620+711299244@users.noreply.github.com>
2017-11-06 11:16:29 +0000

    Add files via upload

---
+++ Project/code/switch_on_off.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SerialPortP
+{
+    public partial class switch_on_off : Form
+    {
+        public switch_on_off()
+        {
+            InitializeComponent();
+            serialPort1.Open();
+        }
+
+        private void switch_on_off_Load(object sender, EventArgs e)
+        {
+   
+        }
+
+        private void imageSlider1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("A");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("J");
+        }
+    }
+}
